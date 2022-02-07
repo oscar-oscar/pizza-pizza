655895d6190bad46579401c6bef5f09e92650338
oscar-oscar <99150237+oscar-oscar@users.noreply.github.com>
2022-02-06 19:32:12 -0600

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,35 @@
+body {
+  background-color:red;
+}
+
+h1 {
+  color: white;
+  font-size:36px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+img {width:600px;
+}
+
+p, li {
+  color:white;
+  font-family: arial, helvetica, sans-serif;
+  
+      
+}
+
+.header {
+  background: black;
+  border: 1px solid black;
+  padding-left:10px;
+  ;
+}
+
+.footer {
+  background:black;
+  color: white;
+  border: 1px solid black;
+  padding-left:10px;
+  font-family: arial, helvetica, sans-serif;
+}
+
